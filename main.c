e0024ceaf574eef9ee92a5b2a8820b6d1082cbd5
Alexander Crisler <alexcrisler@gmail.com>
2023-10-17 21:00:19 -0700

    Read raw zip file

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-	printf("hello world!");
+	printf("Start of Program");
+
+	FILE *fptr = fopen("AlicesAdventuresInWonderland.epub", "r");
+
+	if (fptr == NULL) {
+		printf("ERROR");
+		exit(1);
+	}
+
+	char c = fgetc(fptr);
+	while (c != EOF) {
+		printf("%C", c);
+		c = fgetc(fptr);
+	}
+
 	return 0;
 }
 
